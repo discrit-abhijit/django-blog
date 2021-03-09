11837241dbdf39440f3b6ffe94ac028047acd2d7
discrit abhijit <abhi.ds04@icloud.com>
2021-03-10 03:51:50 +0600

    Added user authentication, login, and logout.

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,8 @@ from .forms import PostForm
 
 from django.shortcuts import redirect
 
+from django.contrib.auth.decorators import login_required
+
 # Create your views here. 
 # A view is a place where we put the "logic" of our application.
 # Views are just Python functions that are a little bit more complicated 
@@ -22,6 +24,8 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+
+@login_required
 def post_new(request):
 	if request.method == "POST":
 		form = PostForm(request.POST)
@@ -38,7 +42,7 @@ def post_new(request):
 	return render(request, 'blog/post_edit.html', {'form': form})
 
 
-	
+@login_required	
 def post_edit(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 
@@ -55,17 +59,19 @@ def post_edit(request, pk):
 	return render(request, 'blog/post_edit.html', {'form': form})
 
 
+@login_required
 def post_draft_list(request):
 	posts = Post.objects.filter(published_date__isnull = True).order_by('created_date')
 	return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 
+@login_required
 def post_publish(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	post.publish()
 	return redirect('post_detail', pk=pk) 
 
-
+@login_required
 def post_remove(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	post.delete()
